1ecc42de22db34490f89ced805208a9b8bced60b
yanickrochon <yanick.rochon@gmail.com>
2014-04-04 15:00:54 -0400

    Fixed evolution async function bug when defining new mappers

--- test/lib/evolution.test.js
+++ test/lib/evolution.test.js
@@ -5,6 +5,7 @@ var EvolutionException = require('../../lib/exceptions').EvolutionException;
 
 var fs = require('co-fs');
 var path = require('path');
+var suspend = require('co-suspend');
 
 
 describe('Test Evolution', function () {
@@ -22,10 +23,13 @@ describe('Test Evolution', function () {
 
   it('should apply all patches', function * () {
     var mapper = Mapper.define('EvoMapper1');
+    var marker = suspend();
 
-    yield Evolution.apply(mapper, {
+    Evolution.apply(mapper, {
       path: fixturePath
     });
+    mapper.once('evolutionComplete', marker.resume);
+    yield marker.wait();
 
     mapper.testEvolution.should.be.eql([1, 2, 3, 4]);
 
@@ -33,38 +37,47 @@ describe('Test Evolution', function () {
 
   it('should apply and rollback', function * () {
     var mapper = Mapper.define('EvoMapper2');
+    var marker = suspend();
 
-    yield Evolution.apply(mapper, {
+    Evolution.apply(mapper, {
       path: fixturePath
     });
+    mapper.once('evolutionComplete', marker.resume);
+    yield marker.wait(500);
 
     mapper.testEvolution.should.be.eql([1, 2, 3, 4]);
 
     delete mapper.testEvolution;
 
     // rollback
-    yield Evolution.apply(mapper, {
+    Evolution.apply(mapper, {
       version: '0.1.0',
       path: fixturePath
     });
+    mapper.once('evolutionComplete', marker.resume);
+    yield marker.wait(500);
 
     mapper.testEvolution.should.be.eql([-4, -3, -2]);
 
     delete mapper.testEvolution;
 
-    yield Evolution.apply(mapper, {
+    Evolution.apply(mapper, {
       version: '0.1.1',
       path: fixturePath
     });
+    mapper.once('evolutionComplete', marker.resume);
+    yield marker.wait(500);
 
     mapper.should.not.have.property('testEvolution');
 
     delete mapper.testEvolution;
 
-    yield Evolution.apply(mapper, {
+    Evolution.apply(mapper, {
       version: '0.0.0',
       path: fixturePath
     });
+    mapper.once('evolutionComplete', marker.resume);
+    yield marker.wait(500);
 
     mapper.testEvolution.should.be.eql([-1]);
 
@@ -72,16 +85,19 @@ describe('Test Evolution', function () {
 
   it('should fail with invalid version', function * () {
     var mapper = Mapper.define('EvoMapper3');
+    var marker = suspend();
     var versions = [
       undefined, false, true, null, '', '1.0', 'a.b.c', /1\.2\.3/, {}, [], function () {}
     ];
 
     for (var v in versions) {
       try {
-        yield Evolution.apply(mapper, {
+        Evolution.apply(mapper, {
           version: versions[v],
           path: fixturePath
         });
+        mapper.once('evolutionComplete', marker.resume);
+        yield marker.wait(500);
 
         throw new Error('Should have thrown EvolutionException with value `' + String(versions[v]) + '`');
       } catch (e) {
@@ -92,15 +108,18 @@ describe('Test Evolution', function () {
 
   it('should fail with invalid path', function * () {
     var mapper = Mapper.define('EvoMapper4');
+    var marker = suspend();
     var invalidPaths = [
       undefined, null, false, true, '', 0, 1, /foo/, {}, [], function () {}, '/path/to/invalid/file'
     ];
 
     for (var p in invalidPaths) {
       try {
-        yield Evolution.apply(mapper, {
+        Evolution.apply(mapper, {
           path: invalidPaths[p]
         });
+        mapper.once('evolutionComplete', marker.resume);
+        yield marker.wait(500);
 
         throw new Error('Should have thrown EvolutionException with value `' + String(invalidPaths[p]) + '`');
       } catch (e) {
